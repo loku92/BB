ef88b1515ee50d5f058d1e7e22dcf8e98e3d2d39
Daniel Pawłowski <danielpawlow@gmail.com>
2014-11-15 21:45:30 +0100

    OneDrive API updates: *OneDriveSyncPage.xaml.cs - removed buggy code *Common/OndeDriveManager.cs - added with directory creation
    Signed-off-by: Daniel Pawłowski <danielpawlow@gmail.com>

---
+++ BassBooster/BassBooster/Common/OneDriveManager.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Live;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BassBooster.Common
+{
+    public class OneDriveManager
+    {
+        public static LiveConnectClient _client = null;
+
+        public async static Task<string> CreateDirectoryAsync()
+        {
+            string folderId = null;
+            string folderName = "BBLyrics";
+            var query = "me/skydrive/files?filter=folders,albums";
+            var opResult = await OneDriveManager._client.GetAsync(query);
+            dynamic result = opResult.Result;
+
+            foreach (dynamic folder in result.data)
+            {
+                if (folder.name.ToLowerInvariant() == folderName.ToLowerInvariant())
+                {
+                    folderId = folder.id;
+                    break;
+                }
+            }
+
+            if (folderId == null)
+            {
+                try
+                {
+                    var folderData = new Dictionary<string, object>();
+                    folderData.Add("name", "BBLyrics");
+                    LiveOperationResult operationResult = await OneDriveManager._client.PostAsync("me/skydrive", folderData);
+                    result = operationResult.Result;
+                    return result.id;
+                }
+                catch (LiveConnectException exc)
+                {
+                    return null;
+                }
+            }
+
+            return folderId;
+        }
+
+        //public async static Task<string> DownloadFileAsync(LiveConnectClient client, string directory, string fileName)
+        //{
+        //    string skyDriveFolder = await CreateOrGetDirectoryAsync(client, directory, "me/skydrive");
+        //    var result = await client.BackgroundDownloadAsync(skyDriveFolder);
+
+        //    var operation = await client.GetAsync(skyDriveFolder + "/files");
+
+        //    var items = operation.Result["data"] as List<object>;
+        //    string id = string.Empty;
+
+        //    // Search for the file - add handling here if File Not Found
+        //    foreach (object item in items)
+        //    {
+        //        IDictionary<string, object> file = item as IDictionary<string, object>;
+        //        if (file["name"].ToString() == fileName)
+        //        {
+        //            id = file["id"].ToString();
+        //            break;
+        //        }
+        //    }
+
+        //    var downloadResult = await client.GetAsync(string.Format("{0}/content", id));
+
+        //    //var reader = new StreamReader(downloadResult.Stream);
+        //    //string text = await reader.ReadToEndAsync();
+        //    return null;
+        //}
+    }
+}
--- BassBooster/BassBooster/OneDriveSyncPage.xaml.cs
+++ BassBooster/BassBooster/OneDriveSyncPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Live;
+﻿using BassBooster.Common;
+using Microsoft.Live;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -25,25 +26,11 @@ namespace BassBooster
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class OneDriveSyncPage : Page
-    {
-        public LiveConnectClient client ;
-        public LiveAuthClient authClient;
-        public string FolderName
-        {
-            get { return Package.Current.Id.Name; }
-        }
-
-        ApplicationDataContainer settings = ApplicationData.Current.RoamingSettings;
-        public string FolderId
-        {
-            get { return this.settings.Values["FolderId"].ToString(); }
-            private set { this.settings.Values["FolderId"] = value; }
-        }
-
+    {      
         public OneDriveSyncPage()
         {
             this.InitializeComponent();
-            authClient = new LiveAuthClient();
+            bb.Visibility = Visibility.Collapsed;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -53,71 +40,39 @@ namespace BassBooster
 
         private async void LiveLoginButton_Click(object sender, RoutedEventArgs e)
         {
-            //if (!MainPage.LoggedIn) { 
-            bool connected = false;
-            try
+            if (OneDriveManager._client == null)
             {
-                
-                LiveLoginResult result = await authClient.LoginAsync(new string[] { "wl.signin", "wl.skydrive" });
-
-                if (result.Status == LiveConnectSessionStatus.Connected)
+                bool connected = false;
+                try
                 {
-                    connected = true;
-                    client = new LiveConnectClient(result.Session);
-                    var meResult = await client.GetAsync("me");
-                    dynamic meData = meResult.Result;
+                    var authClient = new LiveAuthClient();
+                    LiveLoginResult result = await authClient.LoginAsync(new string[] { "wl.signin", "wl.skydrive", "wl.skydrive_update" });
+
+                    if (result.Status == LiveConnectSessionStatus.Connected)
+                    {
+                        connected = true;
+                        OneDriveManager._client = new LiveConnectClient(result.Session);
+                        var meResult = await OneDriveManager._client.GetAsync("me");
+                        dynamic meData = meResult.Result;
+                    }
                 }
+                catch (LiveAuthException ex)
+                {
+                    LiveLoginButton.Content = "Retry";
+                }
+                catch (LiveConnectException ex)
+                {
+                    LiveLoginButton.Content = "Retry";
+                }                
             }
-            catch (LiveAuthException ex)
-            {
-                StatusBlock.Text = "LiveAuthException";
-                Debug.WriteLine("Exception during sign-in: {0}", ex.Message);
-            }
-            catch (LiveConnectException ex)
-            {
-                StatusBlock.Text = "LiveConnectException";
-            }
-           // MainPage.LoggedIn = true;
             LiveLoginButton.Content = "Log out";
-            //}
-            
+            bb.Visibility = Visibility.Visible;
         }
-        private async void CreateFolder_Executed()
-        {
-            try
-            {
-                // The overload with a String expects JSON, so this does not work:
-                // LiveOperationResult lor = await client.PostAsync("me/skydrive", Package.Current.Id.Name);
+        
 
-                // The overload with a Dictionary accepts initializers:
-                LiveOperationResult lor = await client.PostAsync("me/skydrive", new Dictionary<string, object>() { { "name", this.FolderName } });
-                dynamic result = lor.Result;
-                string name = result.name;
-                string id = result.id;
-                this.FolderId = id;
-                Debug.WriteLine("Created '{0}' with id '{1}'", name, id);
-            }
-            catch (LiveConnectException ex)
-            {
-                if (ex.HResult == -2146233088)
-                {
-                    Debug.WriteLine("The folder already existed.");
-                }
-                else
-                {
-                    Debug.WriteLine("Exception during folder creation: {0}", ex.Message);
-                }
-            }
-            catch (Exception ex)
-            {
-                // Get the code monkey's attention.
-                Debugger.Break();
-            }
-        }
-
-        private void bb_Click(object sender, RoutedEventArgs e)
+        private async void bb_Click(object sender, RoutedEventArgs e)
         {
-            CreateFolder_Executed();
+            OneDriveManager.CreateDirectoryAsync();
         }
 
     }
