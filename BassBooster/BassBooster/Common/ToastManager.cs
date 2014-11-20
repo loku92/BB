7e6f23a339ccc8ce45a3a7ea37a6bbad089c5a5b
Daniel Pawłowski <danielpawlow@gmail.com>
2014-11-20 21:42:56 +0100

    *Common/ToastManager.cs - added class managing toasts
    Signed-off-by: Daniel Pawłowski <danielpawlow@gmail.com>

---
+++ BassBooster/BassBooster/Common/ToastManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace BassBooster.Common
+{
+    public class ToastManager
+    {
+        public static bool isEnabled = false;
+        public static void ShowToast(string title)
+        {
+            if (isEnabled)
+            {
+                ToastTemplateType toastTemplate = ToastTemplateType.ToastImageAndText01;
+                XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+                XmlNodeList toastTextElements = toastXml.GetElementsByTagName("text");
+                toastTextElements[0].AppendChild(toastXml.CreateTextNode(title));
+                IXmlNode toastNode = toastXml.SelectSingleNode("/toast");
+                ((XmlElement)toastNode).SetAttribute("duration", "short");
+                ToastNotification toast = new ToastNotification(toastXml);
+                ToastNotificationManager.CreateToastNotifier().Show(toast);
+            }
+        }
+    }
+}
