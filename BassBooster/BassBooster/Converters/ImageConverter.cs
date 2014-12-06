23868c5694759505c52dbd55b6b72ec889e314ef
Daniel Pawłowski <danielpawlow@gmail.com>
2014-12-06 20:16:11 +0100

    *MainPage.xaml changed Playlist from ListView to GridView .
    Signed-off-by: Daniel Pawłowski <danielpawlow@gmail.com>

---
+++ BassBooster/BassBooster/Converters/ImageConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace BassBooster.Converters
+{
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Random rnd = new Random((int)DateTime.Now.Ticks);
+            return "Assets/" + rnd.Next(17) +".png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return true;
+        }
+    }
+}
